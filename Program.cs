c249cb83da2f51223aab1ddd7d671e6b1cd89d92
vee15 <vaishnavicvhatkar@gmail.com>
2023-07-16 12:28:41 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the number of book details you want to store: ");
+            int numBooks = Convert.ToInt32(Console.ReadLine());
+
+            Book[] books = new Book[numBooks];
+
+            Console.WriteLine("\nInsert the information of books:");
+
+
+            for (int i = 0; i < numBooks; i++)
+            {
+                Console.WriteLine($"Information of book {i + 1}:");
+                Console.Write("Input name of the book: ");
+                string title = Console.ReadLine();
+                Console.Write("Input the author: ");
+                string author = Console.ReadLine();
+
+                books[i] = new Book { Title = title, Author = author };
+                Console.WriteLine();
+            }
+
+            Console.WriteLine("Book Details are:");
+            for (int i = 0; i < numBooks; i++)
+            {
+                Console.WriteLine($"{i + 1}: Title = {books[i].Title}, Author = {books[i].Author}");
+            }
+        }
+    }
+    class Book
+    {
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+    }
+}
